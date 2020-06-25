058d796ca731f529190e8bde8a32c715b0b6ee99
Matheus Santos Moreira <matheussm301@gmail.com>
2020-06-25 20:00:32 -0300

    chore: Remove isNullable da migration Appointments

--- src/database/migrations/1592965900931-CreateAppointments.ts
+++ src/database/migrations/1592965900931-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1592965900931
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
