06d5bce34fa994bee0ead31d5d3bf3f231c85101
Matheus Santos Moreira <matheussm301@gmail.com>
2020-06-25 20:05:48 -0300

    chore: Cria campo de password

--- src/database/migrations/1593125225840-CreateUsers.ts
+++ src/database/migrations/1593125225840-CreateUsers.ts
@@ -23,6 +23,10 @@ export default class CreateUsers1593125225840 implements MigrationInterface {
             isUnique: true,
           },
           {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()',
