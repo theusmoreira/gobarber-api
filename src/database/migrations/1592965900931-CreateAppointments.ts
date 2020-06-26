4ad33e40f21ce318e0dbc91f433a14081c491b60
Matheus Santos Moreira <matheussm301@gmail.com>
2020-06-25 21:42:09 -0300

    chore: Adiciona colunas na migration e model

--- src/database/migrations/1592965900931-CreateAppointments.ts
+++ src/database/migrations/1592965900931-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1592965900931
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
