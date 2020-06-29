71dd2207d1bdb2a0b279dd3f36922b7dd0798c03
Matheus Santos <matheussm301@gmail.com>
2020-06-28 22:56:20 -0300

    chore: Lida com errors

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -18,22 +18,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parseDate = parseISO(date);
+  const parseDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parseDate,
-    });
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parseDate,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
