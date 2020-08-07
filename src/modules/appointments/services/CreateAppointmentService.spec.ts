6402ef332c31b4fa877c966687b43aa3aa5cef4a
Matheus Santos <matheussm301@gmail.com>
2020-08-07 08:23:39 -0300

    test: Create test of Appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('1234');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 8, 7, 8);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '1234',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '1234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
