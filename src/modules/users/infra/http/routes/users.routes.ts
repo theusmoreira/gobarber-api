b69e73717129443961b03dd4f46c5b9a595e5f2f
Matheus Santos <matheussm301@gmail.com>
2020-09-12 20:16:26 -0300

    feat: Valid data

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import uploadConfig from '@config/upload';
 
 import UsersController from '../controllers/UsersCrontroller';
@@ -13,7 +14,17 @@ const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
