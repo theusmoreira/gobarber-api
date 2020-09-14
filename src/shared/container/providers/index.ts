2c97d3ca1a96b058ce5b61a49d7bd69ccdbf827d
Matheus Santos <matheussm301@gmail.com>
2020-09-13 22:12:18 -0300

    chore: Organizing containers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,12 +1,3 @@
-import { container } from 'tsyringe';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
+import './StorageProvider';
 import './MailTemplateProvider';
 import './MailProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
