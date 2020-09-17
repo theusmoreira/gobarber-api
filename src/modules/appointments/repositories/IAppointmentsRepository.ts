523524a7bceb0f8cb0092847eaa1832e6aaa3cce
Matheus Santos <matheussm301@gmail.com>
2020-09-16 21:44:46 -0300

    feat: Add provider_id as a parameter in findByDate

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import IFindAllDayFromProviderDTO from '../dtos/IFindAllDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllMonthFromProviderDTO,
   ): Promise<Appointment[]>;
